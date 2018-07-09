24c331469d475289edfb95af2797371b00c65da3
ZhangChi <jeorch@foxmail.com>
2018-07-09 19:30:59 +0800

    add product aggregation

--- js/max_data_mapreduce_job.js
+++ js/max_data_mapreduce_job.js
@@ -1,34 +1,84 @@
-function getSalesAgg(collection, scope){
-    db[collection].mapReduce(
+db.getCollectionNames().forEach(
+    function(collection) {
+        db[collection].mapReduce(
             function() { emit(this.MARKET, this.f_sales); },
             function(key, values) { return Array.sum(values); }, {
                 query: { MARKET: { $exists: true } },
                 out: {
-                    replace: collection + scope,
+                    replace: collection + "_NATION_SALES",
                     db: "pharbers-max-aggregation"
                 }
             }
         );
-}
-
-function getCompanySalesAgg(collection, scope){
-    db[collection].mapReduce(
+        db[collection].mapReduce(
             function() { emit(this.MARKET, this.f_sales); },
             function(key, values) { return Array.sum(values); }, {
                 query: { belong2company: 1 },
                 out: {
-                    replace: collection + scope,
+                    replace: collection + "_NATION_COMPANY_SALES",
                     db: "pharbers-max-aggregation"
                 }
             }
         );
-}
-
-db.getCollectionNames().forEach(
-    getSalesAgg(collection, "_NATION_SALES");
-    getSalesAgg(collection, "_PROVINCE_SALES");
-    getSalesAgg(collection, "_CITY_SALES");
-    getCompanySalesAgg(collection, "_NATION_COMPANY_SALES");
-    getCompanySalesAgg(collection, "_PROVINCE_COMPANY_SALES");
-    getCompanySalesAgg(collection, "_CITY_COMPANY_SALES");
+        db[collection].mapReduce(
+            function() { emit(this.Province, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { MARKET: { $exists: true } },
+                out: {
+                    replace: collection + "_PROVINCE_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+        db[collection].mapReduce(
+            function() { emit(this.Province, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { belong2company: 1 },
+                out: {
+                    replace: collection + "_PROVINCE_COMPANY_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+        db[collection].mapReduce(
+            function() { emit(this.City, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { MARKET: { $exists: true } },
+                out: {
+                    replace: collection + "_CITY_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+        db[collection].mapReduce(
+            function() { emit(this.City, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { belong2company: 1 },
+                out: {
+                    replace: collection + "_CITY_COMPANY_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+        db[collection].mapReduce(
+            function() { emit(this.Product, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { MARKET: { $exists: true } },
+                out: {
+                    replace: collection + "_Product_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+        db[collection].mapReduce(
+            function() { emit(this.Product, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { belong2company: 1 },
+                out: {
+                    replace: collection + "_Product_COMPANY_SALES",
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+    }
 );
