38e6fc043453baf157fbaa52561ad3b8992118c4
ZhangChi <jeorch@foxmail.com>
2018-07-09 20:37:24 +0800

    统一命名

--- js/max_data_mapreduce_job.js
+++ js/max_data_mapreduce_job.js
@@ -65,7 +65,7 @@ db.getCollectionNames().forEach(
             function(key, values) { return Array.sum(values); }, {
                 query: { MARKET: { $exists: true } },
                 out: {
-                    replace: collection + "_Product_SALES",
+                    replace: collection + "_PRODUCT_SALES",
                     db: "pharbers-max-aggregation"
                 }
             }
@@ -75,7 +75,7 @@ db.getCollectionNames().forEach(
             function(key, values) { return Array.sum(values); }, {
                 query: { belong2company: 1 },
                 out: {
-                    replace: collection + "_Product_COMPANY_SALES",
+                    replace: collection + "_PRODUCT_COMPANY_SALES",
                     db: "pharbers-max-aggregation"
                 }
             }
